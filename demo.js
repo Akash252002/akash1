d972a7a5df137fd07414df793f4e91479cfcb50f
Akash252002 <97967936+Akash252002@users.noreply.github.com>
2022-01-19 12:17:35 +0530

    commit1

---
+++ demo.js
@@ -0,0 +1,8 @@
+
+document.querySelector("button").addEventListener("click",js);
+function js(){
+
+    // prompt("enter your name");
+    // prompt("enter your lover name");
+    var x=Math.floor(Math.random()*100)+1;
+    alert("your love between you and your lover is " +x+"%");}
